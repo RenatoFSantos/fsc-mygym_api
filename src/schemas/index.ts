8dfe2a8d5a15292545957e998d797722fdc10e55
RenatoFSantos <renato.insigne@gmail.com>
2026-03-04 20:54:12 -0300

    feat: add timestampz and delete cascade

---
+++ src/schemas/index.ts
@@ -0,0 +1,31 @@
+import z from 'zod';
+
+import { WeekDay } from '../generated/prisma/enums.js';
+
+export const ErrorSchema = z.object({
+    error: z.string(),
+    code: z.string()
+})
+
+export const WorkoutPlanSchema = z.object({
+    id: z.uuid(),
+    name: z.string().trim().min(1, {
+        error: 'Name is required',
+    }),
+    workoutDays: z.array(z.object({
+        name: z.string().trim().min(1, {
+            error: 'Day name is required',
+        }),
+        weekDay: z.enum(WeekDay).default(WeekDay.MONDAY),
+        isRest: z.boolean().default(false),
+        estimatedDurationInSeconds: z.number().min(1),
+        workoutExercises: z.array(
+            z.object({
+                name: z.string().trim().min(1, { error: 'Exercise name is required' }),
+                order: z.number().min(1),
+                sets: z.number().min(1),
+                reps: z.number().min(1),
+                restTimeInSeconds: z.number().min(1),
+            })),
+    })),
+});
