8e482e0e71f5630d583d075ee01c79901eeaac52
RenatoFSantos <renato.insigne@gmail.com>
2026-03-30 20:45:41 -0300

    feat: add pino-pretty

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,22 @@ import { workoutPlanRoutes } from './routes/workout-plan.js';
 
 
 
+const envToLogger = {
+  development: {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        translateTime: "HH:MM:ss Z",
+        ignore: 'pid,hostname',
+      }
+    }
+  },
+  production: true,
+  test: false,
+} 
+
 const app = Fastify({
-  logger: true
+  logger: envToLogger[env.NODE_ENV]
 })
 
 app.setValidatorCompiler(validatorCompiler);
